acce8646123084e3131c34db299942d6e6158f47
debankur123 <debankurbhattacharjee784@gmail.com>
2022-11-04 21:53:04 +0530

    Azure committed

---
+++ BulkyBook/obj/Release/net6.0/win-x86/PubTmp/EFSQLScripts/BulkyBook.Data.AppDbContext.sql
@@ -0,0 +1,35 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20221030154143_AddCategoryToDatabase')
+BEGIN
+    CREATE TABLE [Categories] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [Displayorder] int NOT NULL,
+        [CreatedOn] datetime2 NOT NULL,
+        CONSTRAINT [PK_Categories] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20221030154143_AddCategoryToDatabase')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20221030154143_AddCategoryToDatabase', N'6.0.10');
+END;
+GO
+
+COMMIT;
+GO
+
